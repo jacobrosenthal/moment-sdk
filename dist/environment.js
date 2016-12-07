5b8b5f350c86a185c564cf7433f94ad27e2b99c1
Shantanu Bala <shantanubala@gmail.com>
2016-12-07 14:35:21 -0700

    added environment files for distribution

--- dist/environment.js
+++ dist/environment.js
@@ -1,3 +1,3 @@
 (function(){
-'use strict';
+'use strict';var d=window.Moment=window.Moment||{};d.a={};d.on=function(a,b){var c=d.a;c.hasOwnProperty(a)?c[a].push(b):c[a]=[b]};d.off=function(a,b){var c=d.a;c.hasOwnProperty(a)&&(a=c[a],b=a.indexOf(b),0<=b&&a.splice(b,1))};d.once=function(a,b){function c(){b();d.off(a,c)}d.on(a,c)};d.trigger=function(a){var b=d.a,c;if(b.hasOwnProperty(a))for(a=b[a],c=a.length,b=0;b<c;b+=1)a[b]()};
 }).call(this)
