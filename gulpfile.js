b6ce696072438d00d63431e918791f9b5aef45bd
Shantanu Bala <shantanubala@gmail.com>
2016-12-16 08:29:35 -0700

    proper compilation procedure

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,25 @@ var gulp = require('gulp'),
     closureCompiler = require('google-closure-compiler').gulp();
 
 gulp.task('compile', function () {
-    return gulp.src('./src/**/*.js', {base: './'})
-        .pipe(closureCompiler({
-          compilation_level: 'ADVANCED',
-          warning_level: 'VERBOSE',
-          language_in: 'ECMASCRIPT6_STRICT',
-          language_out: 'ECMASCRIPT5_STRICT',
-          output_wrapper: '(function(){\n%output%\n}).call(this)',
-          js_output_file: 'environment.js'
-        }))
-        .pipe(gulp.dest('./dist'));
+  return closureCompiler([
+        '--js', './src/_init.js',
+        '--js', './src/actuators.js',
+        '--js', './src/battery.js',
+        '--js', './src/color.js',
+        '--js', './src/easing.js',
+        '--js', './src/events.js',
+        '--js', './src/haptics.js',
+        '--js', './src/imu.js',
+        '--js', './src/led.js',
+        '--js', './src/timeline.js',
+        '--js', './src/timeout.js',
+        '--language_in', 'ECMASCRIPT6_STRICT',
+        '--language_out', 'ECMASCRIPT5_STRICT',
+        '--warning_level', 'VERBOSE',
+        '--output_wrapper', '(function(){\n%output%\n}).call(this)',
+        '--compilation_level', 'ADVANCED',
+        '--js_output_file', 'environment.js'
+      ])
+      .src() // needed to force the plugin to run without gulp.src
+      .pipe(gulp.dest('./dist'));
 });
