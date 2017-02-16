6c4b0f26b4095d71e9f46eaab3df4807cfdf583a
Shantanu Bala <shantanubala@gmail.com>
2017-02-16 10:16:22 -0700

    added plugins.js

---
+++ src/plugins.js
@@ -0,0 +1,94 @@
+/** Copyright (c) 2016 Somatic Labs. All Rights Reserved.
+  *
+  * This program is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU General Public License as published by
+  * the Free Software Foundation, either version 3 of the License, or
+  * (at your option) any later version.
+  *
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+  *
+  * You should have received a copy of the GNU General Public License
+  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+  *
+  * The information contained herein is property of Somatic Technologies, Inc.
+  *
+  */
+
+(function () {
+
+// local variable reference to global `Moment` object
+var Moment = Function('return this')()['Moment'];
+
+// global map of all installed plugins
+var plugins = {};
+
+/** Provides an constructor to encapsulate plugins within the SDK.
+  *
+  * @constructor
+  * @name Plugin
+  * @param {Object} plugin - The methods for the plugin
+  */
+function Plugin(plugin) {
+    for (var key in plugin) {
+        if (plugin.hasOwnProperty(key)) {
+            this[key] = plugin[key];
+        }
+    }
+
+    this._init();
+}
+
+/** Extends the global Moment namespace with additional functionality that can
+  * be easily enabled, disabled, or removed later.
+  *
+  * @method
+  * @memberof Moment
+  *
+  * @param {String} name - The name of the plugin
+  * @param {Object} plugin - The methods for the plugin
+  */
+Moment['extend'] = function(name, plugin) {
+    plugins[name] = new Plugin(plugin);
+};
+
+/** Removes a plugin that was previously installed using `Moment.extend()`.
+  * This function permanently removes the plugin, and the plugin must be fully
+  * re-initialized using `Moment.extend()` to be reinstalled.
+  *
+  * @method
+  * @memberof Moment
+  *
+  * @param {String} name - The name of the plugin to remove
+  */
+Moment['remove'] = function(name) {
+    plugins[name]._remove();
+    delete plugins[name];
+}
+
+
+Plugin['prototype']['init'] = function () {};
+Plugin['prototype']['remove'] = function () {};
+Plugin['prototype']['events'] = {};
+
+Plugin['prototype']._init = function () {
+    this['init']();
+    var events = this['events'];
+
+    for (var key in events) {
+        Moment['on'](key, events[key]);
+    }
+}
+
+Plugin['prototype']._remove = function () {
+    this['remove']();
+    var events = this['events'];
+
+    for (var key in events) {
+        Moment['off'](key, events[key]);
+    }
+}
+
+})();
